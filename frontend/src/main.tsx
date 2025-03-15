5c693848765458cb3dc4da2cb45b2e57f8ec8a6f
CN-14NXP-FUZ <22a1001d0040@students.hou.edu.vn>
2025-03-15 15:46:29 +0700

    Sửa lỗi cú pháp trong main.tsx và đường dẫn trong index.html

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.css';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
